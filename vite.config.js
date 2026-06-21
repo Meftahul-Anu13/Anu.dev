27019717d5cd3216ea32d09468d0fc7148193b7e
Anonna <meftahul@iut-dhaka.edu>
2026-06-21 11:59:27 +0600

    chore: trigger deployment with VITE_WEB3FORMS_ACCESS_KEY secret

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-// Trigger redeployment after repository visibility change
+// Trigger build after GitHub Secrets added
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
 
 
